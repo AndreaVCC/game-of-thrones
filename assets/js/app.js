b4272ddf81f179852e2c6708af8043bb634686ac
vladi2468 <a.carreocoppo2@gmail.com>
2019-01-19 22:29:09 -0300

    modificando id de los index, añadiendo funcion de filtrado de temporada 1 en app.js, y agregando variables para filtrar temporada 1 en main2.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
-/**
- * 
- * Acá va solo la lógica
- * 
- */
+const filterTemporada = (data2) => {
+    const filteredTemporada = data2.filter(element2 => {
+      return element2.seasonNum === 1
+    })
+    return filteredTemporada;
+     };
+
+     window.filterTemporada=filterTemporada;
--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,9 +1,10 @@
- // MOSTRANDO DATA CAPITULOS 
-
 
  const data2 = (EPISODES.episodes)
  const containerRoot2 = document.getElementById('root2');
-const mostrarData2 = (data2) => {
+ const selectTemporada = document.getElementById('episodeTitle');
+
+// MOSTRANDO DATA CAPITULOS 
+ const mostrarData2 = (data2) => {
  let result2 = ''; 
  data2.forEach(element2 => {   
    result2 = containerRoot2.innerHTML += `
@@ -29,7 +30,47 @@ const mostrarData2 = (data2) => {
  );
  return result2;
  }
- window.onload = mostrarData2(data2);
+ 
+
+
+
+ //FILTRADO CON TAGS
+ selectTemporada.addEventListener('change', () => {
+    let condition = selectTemporada.value
+    let filteredTemporada = filterTemporada(data2, condition);
+    
+    // limpio div
+      containerRoot2.innerHTML = '';
+      filteredTemporada.forEach(element2 => {
+      containerRoot2.innerHTML += `
+      <div class="card">
+  
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">
+        <p clas="nombreepisodio"><h6>${element2.episodeTitle}</h6></p>
+        <p>Capítulo ${element2.episodeNum} - Temporada ${element2.seasonNum}</p>
+        </span>
+      </div>
+    
+      <div class="card-contentIcon">
+      <i class="material-icons activator">more_horiz</i>
+      </div>
+      <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+        <p>${element2.episodeDescription}</p>
+      </div>
+    </div>` 
+    })
+  })
+
+
+
+  window.onload = mostrarData2(data2);
+
+
+
+
+
 
 
 
