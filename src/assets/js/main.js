2ab481f135e2d2fc93cd7be466e4354d4c7480fd
vladi2468 <a.carreocoppo2@gmail.com>
2019-01-21 08:43:37 -0300

    Initial src subtree commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,50 @@
+//Creo una Variable para entrar y guardar la data
+const data = (DATA.characters)
+//console.log(data)
+
+//Aqui se guardara todo                   Se llama root del HTML
+const containerRoot = document.getElementById('root');
+
+// MOSTRANDO DATA PERSONAJES 
+  const mostrarData = (data) => {
+    let result = ''; 
+    data.forEach(element => {   
+      result = containerRoot.innerHTML += `
+      <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <div class="img1"><img class="activator" src="${element.characterImageFull}" alt=""></div>
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4"><h6>${element.characterName}</h6></span>
+      </div>
+      <div class="card-contentIcon">
+      <i class="material-icons activator">add_circle_outline</i>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${element.actorName}<i class="material-icons right">close</i></span>
+        <p> <A href="https://es.wikipedia.org/wiki/David_Rintoul">Biografía.</A></p>
+      </div>
+    </div>
+       `
+    }
+    );
+    return result;
+    }
+    window.onload = mostrarData(data);
+
+
+
+   
+
+
+
+
+
+
+
+
+
+     /*INICIALIZA EL MATERIALIZE*/
+   document.addEventListener('DOMContentLoaded', function() {
+    window.M.AutoInit();
+  });
